dde7d28655f11244682eba0075cb04e5b0671620
sshankar <sshankar@iastate.edu>
2019-03-12 11:46:27 -0500

    google search now works, also made prompt fixes in UI

--- ChatBox/src/UI.java
+++ ChatBox/src/UI.java
@@ -7,8 +7,11 @@ public class UI
         Scanner scan = new Scanner(System.in);
 
         while(true)
-        {
-            System.out.println("Hi there! How may I help you?");
+        {   System.out.println("Hello! My name is ChatBox!");
+        	System.out.println("    -To ask for the weather type: weather in \"city\" OR temp in \"city\"");
+        	System.out.println("        Example: what is the temp in Ames");
+        	System.out.println("    -To ask search for pictures Type: search image \"what you want to search\", \\\"number of images\\\"");
+			System.out.println("        Example: search image dogs, 5; this will return 5 different pictures of dogs");
             String query = scan.nextLine();
 
             if(query.toLowerCase().contains("bye"))
@@ -24,7 +27,7 @@ public class UI
             }
             else
             {
-                System.out.println("I dunno");
+                System.out.println("Sorry, I am unable to accomplish this task right now.");
             }
 
             System.out.println();
--- ChatBox/src/features/GoogleImageSearch.java
+++ ChatBox/src/features/GoogleImageSearch.java
@@ -1,20 +1,74 @@
 package features;
 
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class GoogleImageSearch extends Feature
-{
+{   String toSearch = "";
+	public static final String GOOGLE_SEARCH_URL = "https://www.google.com/search";
+	int numLinksToReturn=0;
 
 	public GoogleImageSearch(String query)
 	{
+		/*This will get the query which is what the user wants to search and the number of top 
+		 * results that the user wants returned and then assign each of them to the corresponding
+		 * public value to be used in the response method. 
+		*/
 		super(query);
-		System.out.print(query);
-		// TODO Auto-generated constructor stub
+	
+		query= query.replaceAll("\\s+", "");
+		String toInt="";
+		int toStopSubstring = query.length();
+		
+		
+	    for(int i= 0; i<query.length(); i++) {
+	    	if(query.charAt(i)==',') {
+	    
+	    		toInt=query.substring(i+1,query.length());
+	    		
+	    		toStopSubstring=i;
+	    	}
+	    }
+		
+		toSearch=query.substring(0,toStopSubstring);
+		if(toInt.length()==0) {
+			numLinksToReturn= 3;
+		}
+		else {
+			numLinksToReturn= Integer.parseInt(toInt);
+		}
+		
 	}
 
 	@Override
-	public String setResponse() 
-	{
-		// TODO Auto-generated method stub
-		return "Hi Shreya";
+	public String setResponse()
+	{ //gets the google search URL using the query and number from constructor.
+		String searchURL = GOOGLE_SEARCH_URL + "?q="+toSearch+"&num="+numLinksToReturn;
+		// A user agent must be used to get data
+		Document doc=null;
+		try {
+			doc = Jsoup.connect(searchURL).userAgent("Mozilla/5.0").get();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		// Adds number of results wanted to "results" and goes through each result and print it out.
+		String toReturn= "";
+		Elements results = doc.select("h3.r > a");
+		for (Element result : results) {
+			String linkHref = result.attr("href");
+			String linkText = result.text();
+			toReturn= toReturn + "\n" + "Text::" + linkText + ", URL::" + linkHref.substring(6, linkHref.indexOf("&"));
+		}
+		
+		return toReturn;
 	}
 
 	@Override
