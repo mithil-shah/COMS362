d59ec57ed3a4c94c192fc65e556b58b5376cf515
Mithil Shah <mithilshah13@gmail.com>
2019-03-13 20:25:00 -0500

    Finished Fetching Song Lyrics

--- ChatBox/src/features/SongLyrics.java
+++ ChatBox/src/features/SongLyrics.java
@@ -1,22 +1,72 @@
+/**
+ * @author Shreya Shankar
+ */
 package features;
 
-public class SongLyrics extends Feature
-{
-	public SongLyrics(String query)
-	{
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.examples.HtmlToPlainText;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class SongLyrics extends Feature {
+	private String response;
+
+	public SongLyrics(String query) {
 		super(query);
+		parseQuery(query);
 	}
 
 	@Override
-	public String setResponse() 
-	{
-		return null;
+	public String setResponse() {
+		return response;
+	}
+
+	private void parseHTML(String baseURL, String parameters) throws IOException {
+		Document doc = Jsoup.connect(baseURL + parameters).get();
+		HtmlToPlainText formatter = new HtmlToPlainText();
+
+		String lyrics = "";
+
+		for (Element row : doc.select("p")) {
+			lyrics = formatter.getPlainText(row);
+			break;
+		}
+
+		for (String s : lyrics.split("\n")) {
+			lyrics = lyrics.replace(s, s.trim());
+
+			if (s.contains("http")) {
+				lyrics = lyrics.replace(s, "");
+			}
+		}
+
+		response = lyrics;
 	}
 
 	@Override
-	public void parseQuery(String query) 
-	{
-		
+	public void parseQuery(String query) {
+		String nonKeywords = "find" + "song" + "lyrics" + "the" + "to" + "get" + "for";
+		String[] words = query.split(" ");
+		String title = "";
+		String artist = "";
+
+		for (int i = 0; i < words.length; i++) {
+			if (nonKeywords.indexOf(words[i].toLowerCase()) < 0 && !words[i].equals("by")) {
+				title += words[i] + "-";
+			} else if (nonKeywords.indexOf(words[i].toLowerCase()) < 0 && words[i].equals("by")) {
+				for (int j = i + 1; j < words.length; j++) {
+					artist += words[j] + "-";
+				}
+				break;
+			}
+		}
+
+		try {
+			parseHTML("https://genius.com/", (artist + title + "lyrics"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 }
