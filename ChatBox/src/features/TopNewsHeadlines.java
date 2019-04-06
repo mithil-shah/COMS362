835a553e56cdaed3c348f7740bc1256765a6d4af
Mithil Shah <mithilshah13@gmail.com>
2019-04-06 15:56:22 -0500

    Minor optimizations

--- ChatBox/src/features/TopNewsHeadlines.java
+++ ChatBox/src/features/TopNewsHeadlines.java
@@ -20,21 +20,20 @@ import configuration.Response;
  *
  */
 
-public class TopNewsHeadlines implements Feature
-{
+public class TopNewsHeadlines implements Feature {
 	/**
-	 * The return string containing the title, author, and URL of top news headlines for the day
+	 * The return string containing the title, author, and URL of top news headlines
+	 * for the day
 	 */
 	private String headlines;
-	
+
 	/**
-	 * Constructs a new TopNewsHeadlines object. Instantiates the headlines string and calls parseQuery()
+	 * Constructs a new TopNewsHeadlines object. Instantiates the headlines string
+	 * and calls parseQuery()
 	 * 
-	 * @param query
-	 * 		The query provided by the user.
+	 * @param query The query provided by the user.
 	 */
-	public TopNewsHeadlines(String query)
-	{
+	public TopNewsHeadlines(String query) {
 		headlines = "";
 		parseQuery(query);
 	}
@@ -42,95 +41,82 @@ public class TopNewsHeadlines implements Feature
 	/**
 	 * Returns the top headlines to the user
 	 * 
-	 * @return Response
-	 * 		The top headlines for the day
+	 * @return Response The top headlines for the day
 	 */
 	@Override
-	public Response setResponse() 
-	{
+	public Response setResponse() {
 		return new Response(headlines);
 	}
 
 	/**
 	 * Nothing needs to be parsed, so we call getHeadlines()
 	 * 
-	 * @param query
-	 * 		The query provided by the user
+	 * @param query The query provided by the user
 	 */
 	@Override
-	public void parseQuery(String query)
-	{
-		try
-		{
+	public void parseQuery(String query) {
+		try {
 			getHeadlines();
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Fetches the top headlines in the US from News API.
 	 * 
-	 * @throws IOException
-	 * 		If the API cannot function properly
+	 * @throws IOException If the API cannot function properly
 	 */
-	private void getHeadlines() throws IOException
-	{
-		//NewsAPI
+	private void getHeadlines() throws IOException {
+		// NewsAPI
 		URL url = new URL("https://newsapi.org/v2/top-headlines?country=us&apiKey=f278c164c5834348885ae1c1d4905eae");
-		
-		//Go to the URL specified above (News API)
-		HttpURLConnection connection =  (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        
-        //Put the JSON object that is returned into a string
-        StringBuilder sb = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String input;
 
-        while((input = br.readLine()) != null)
-        {
-            sb.append(input);
-        }
+		// Go to the URL specified above (News API)
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
 
-        //Close the BufferedReader to prevent leaks.
-        br.close();
-        
-        //Parse the JSON string we just retrieved and appended
-        parseJSONResponse(sb.toString());
+		// Put the JSON object that is returned into a string
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String input;
+
+		while ((input = br.readLine()) != null) {
+			sb.append(input);
+		}
+
+		// Close the BufferedReader to prevent leaks.
+		br.close();
+
+		// Parse the JSON string we just retrieved and appended
+		parseJSONResponse(sb.toString());
 	}
-	
+
 	/**
 	 * Parses the JSON string for title, author, and URLs for the articles returned
 	 * 
-	 * @param response
-	 * 		The JSON string retrieved from getHeadlines()
+	 * @param response The JSON string retrieved from getHeadlines()
 	 */
-	private void parseJSONResponse(String response)
-	{
-		//Creates a JSON element from the provided JSON string from News API 
-        JsonParser parser = new JsonParser();
-        JsonObject data = parser.parse(response).getAsJsonObject();
-        
-        //Gets the articles found in the JSON string
-        JsonArray articles = data.getAsJsonArray("articles");
-        
-        //For each article in the articles array
-        for(JsonElement article : articles)
-        {
-        	//Get the current article as an article object
-        	JsonObject currentArticle = article.getAsJsonObject();
-        	
-        	//From the article object, get these properties of the article
-        	//Title contains title and author
-        	JsonElement title = currentArticle.get("title");
-        	JsonElement url = currentArticle.get("url");
-        	        	
-        	//Append these properties to the return string
-        	headlines += title.getAsString() + "\n" + url.getAsString() + "\n\n";
-        }
+	private void parseJSONResponse(String response) {
+		// Creates a JSON element from the provided JSON string from News API
+		JsonParser parser = new JsonParser();
+		JsonObject data = parser.parse(response).getAsJsonObject();
+
+		// Gets the articles found in the JSON string
+		JsonArray articles = data.getAsJsonArray("articles");
+
+		// For each article in the articles array
+		for (JsonElement article : articles) {
+			// Get the current article as an article object
+			JsonObject currentArticle = article.getAsJsonObject();
+
+			// From the article object, get these properties of the article
+			// Title contains title and author
+			JsonElement title = currentArticle.get("title");
+			JsonElement url = currentArticle.get("url");
+
+			// Append these properties to the return string
+			headlines += title.getAsString() + "\n" + url.getAsString() + "\n\n";
+		}
 	}
-	
+
 }
--- ChatBox/src/features/UnsplashSearch.java
+++ ChatBox/src/features/UnsplashSearch.java
@@ -19,14 +19,13 @@ import com.google.gson.JsonParser;
 import configuration.Response;
 
 /**
- * Retrieves specific number of photos regarding a specific topic 
+ * Retrieves specific number of photos regarding a specific topic
  * 
  * @author Shreya Shankar
  *
  */
 
-public class UnsplashSearch implements Feature
-{
+public class UnsplashSearch implements Feature {
 	/**
 	 * The search query
 	 */
@@ -39,162 +38,140 @@ public class UnsplashSearch implements Feature
 	 * The images that are returned in an arraylist
 	 */
 	private ArrayList<BufferedImage> results;
-	
+
 	/**
-	 * Constructs a new UnsplashSearch object where variables above are initialized, query is parsed, and then images are retrieved
+	 * Constructs a new UnsplashSearch object where variables above are initialized,
+	 * query is parsed, and then images are retrieved
 	 * 
-	 * @param query
-	 * 		The query provided by the user
+	 * @param query The query provided by the user
 	 */
-	public UnsplashSearch(String query)
-	{ 
+	public UnsplashSearch(String query) {
 		toSearch = "";
 		numLinksToReturn = -1;
 		results = new ArrayList<>();
 		parseQuery(query);
-		
-		if(!toSearch.equals("") && numLinksToReturn > 0)
-		{
-			try
-			{
+
+		if (!toSearch.equals("") && numLinksToReturn > 0) {
+			try {
 				getImages(toSearch, numLinksToReturn);
-			}
-			catch (IOException e)
-			{
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	/**
-	 * Retrieves images based off the topic specified by the user and the number of images specified by the user
-	 * 
-	 * @param toSearch
-	 * 		The category of the image
-	 * @param numberOfPics
-	 * 		Number of images to return
-	 * @throws IOException
-	 * 		Thrown if API cannot properly process the request
-	 */
-	public void getImages(String toSearch, int numberOfPics) throws IOException
-	{
-		String searchUrl = "https://api.unsplash.com/search/photos?page=1&per_page=" + numberOfPics + "&query=" + toSearch + "&client_id=84023d7dd975661fbade252d0a70f9bb401be3af6d272c7913c4cd2c66c13524";
-		
-    	//Set up the URL connection and get data from it
-		URL url = new URL(searchUrl);
-        HttpURLConnection connection =  (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-                
-        //Get the data from the JSON file that is returned by the URL
-        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuilder sb = new StringBuilder();
-        String input;
 
-        //Set the JSON data to a string called sb.
-        while((input = br.readLine()) != null)
-        {
-            sb.append(input);
-        }
-        
-        parseJSON(sb.toString());
+	/**
+	 * Retrieves images based off the topic specified by the user and the number of
+	 * images specified by the user
+	 * 
+	 * @param toSearch     The category of the image
+	 * @param numberOfPics Number of images to return
+	 * @throws IOException Thrown if API cannot properly process the request
+	 */
+	public void getImages(String toSearch, int numberOfPics) throws IOException {
+		String searchUrl = "https://api.unsplash.com/search/photos?page=1&per_page=" + numberOfPics + "&query="
+				+ toSearch + "&client_id=84023d7dd975661fbade252d0a70f9bb401be3af6d272c7913c4cd2c66c13524";
+
+		// Set up the URL connection and get data from it
+		URL url = new URL(searchUrl);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+
+		// Get the data from the JSON file that is returned by the URL
+		BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		StringBuilder sb = new StringBuilder();
+		String input;
+
+		// Set the JSON data to a string called sb.
+		while ((input = br.readLine()) != null) {
+			sb.append(input);
+		}
+
+		parseJSON(sb.toString());
 	}
-	
+
 	/**
 	 * Parses the JSON returned by the Unsplash API
 	 * 
-	 * @param response
-	 * 		The JSON string retrieved in getImages()
+	 * @param response The JSON string retrieved in getImages()
 	 */
-	private void parseJSON(String response)
-	{
-    	//Make the JSON string into an object using Gson Library
-        JsonParser parser = new JsonParser();
-        JsonObject data = parser.parse(response).getAsJsonObject();
-        
-        //Find the array in the JSON file called "results"
-        JsonArray results = data.getAsJsonArray("results");
-        
-        //Holds the URLs of the images found
-        ArrayList<String> imagePaths = new ArrayList<>();
-        
-        //Get and store the URLs of small thumbnails
-        for(JsonElement result: results)
-        {
-        	JsonObject currentResult = result.getAsJsonObject();
-        	
-        	JsonElement urls = currentResult.get("urls");
-        	JsonElement urlSmall = urls.getAsJsonObject().get("small");
-        	imagePaths.add(urlSmall.getAsString());
-        }
-        
-        fetchImages(imagePaths);
+	private void parseJSON(String response) {
+		// Make the JSON string into an object using Gson Library
+		JsonParser parser = new JsonParser();
+		JsonObject data = parser.parse(response).getAsJsonObject();
+
+		// Find the array in the JSON file called "results"
+		JsonArray results = data.getAsJsonArray("results");
+
+		// Holds the URLs of the images found
+		ArrayList<String> imagePaths = new ArrayList<>();
+
+		// Get and store the URLs of small thumbnails
+		for (JsonElement result : results) {
+			JsonObject currentResult = result.getAsJsonObject();
+
+			JsonElement urls = currentResult.get("urls");
+			JsonElement urlSmall = urls.getAsJsonObject().get("small");
+			imagePaths.add(urlSmall.getAsString());
+		}
+
+		fetchImages(imagePaths);
 	}
-	
+
 	/**
 	 * Fetches the images from the URLs
 	 * 
-	 * @param paths
-	 * 		The paths of the images found in parseJSON()
+	 * @param paths The paths of the images found in parseJSON()
 	 */
-	private void fetchImages(ArrayList<String> paths)
-	{
+	private void fetchImages(ArrayList<String> paths) {
 		URL url;
 		ArrayList<BufferedImage> images = new ArrayList<>();
-		
-		//Get the images from the URL and convert them into BufferedImage
-		for(String path: paths)
-		{
-			try 
-			{
+
+		// Get the images from the URL and convert them into BufferedImage
+		for (String path : paths) {
+			try {
 				url = new URL(path);
 				images.add(ImageIO.read(url));
-			} 
-			catch (IOException e)
-			{
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		results = images;
 	}
 
 	/**
 	 * Shows the images found to the user
 	 * 
-	 * @return Response
-	 * 		The images found by the API
+	 * @return Response The images found by the API
 	 */
 	@Override
-	public Response setResponse()
-	{
+	public Response setResponse() {
 		return new Response(results);
 	}
 
 	/**
 	 * Parses the query for the topic to search and number of images to return
 	 * 
-	 * @param query
-	 * 		The query provided by the user
+	 * @param query The query provided by the user
 	 */
 	@Override
-	public void parseQuery(String query) 
-	{
-    	//These are words that are probably not the search term
-        String nonKeywords = "search" + "image";
-        
-        //The query is split into keywords
-        String [] words = query.split(" ");
-                
-        //Gets the search term provided by the user by avoiding all words in the nonKeywords string.
-        for(int i = 0; i < words.length-1; i++)
-        {
-        	
-        	if(!nonKeywords.contains(words[i]))
-        	{
-        		toSearch = words[i].substring(0, words[i].length()-1);
-        	}
-        }
+	public void parseQuery(String query) {
+		// These are words that are probably not the search term
+		String nonKeywords = "search" + "image";
 
-		numLinksToReturn = Integer.parseInt(words[words.length-1]);
+		// The query is split into keywords
+		String[] words = query.split(" ");
+
+		// Gets the search term provided by the user by avoiding all words in the
+		// nonKeywords string.
+		for (int i = 0; i < words.length - 1; i++) {
+
+			if (!nonKeywords.contains(words[i])) {
+				toSearch = words[i].substring(0, words[i].length() - 1);
+			}
+		}
+
+		numLinksToReturn = Integer.parseInt(words[words.length - 1]);
 	}
 }
